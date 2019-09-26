e87244c094f711493de0f8762df6cae52dfd0fb7
Dan Marshall <50140623+UntappedCode@users.noreply.github.com>
2019-09-26 14:27:52 -0400

    Update Fight.js

--- Fight.js
+++ Fight.js
@@ -1,15 +1,13 @@
 
-function randomNumber(x) {
-    if ( x > 3) {
+function fight() {
+    var success = Math.floor(Math.random() * 10);
+    console.log(success);
+    if ( success > 3) {
       return document.getElementById("img").src = "https://cdn.pixabay.com/photo/2016/08/17/22/22/pow-1601674_960_720.png";
     } else {
       return document.getElementById("img").src = "https://images.clipartof.com/thumbnails/440628-Royalty-Free-RF-Clip-Art-Illustration-Of-A-Cartoon-Baseball-Batter-Striking-Out.jpg";
     }
   }
 
- function fight(){
-    document.getElementById("demo").innerHTML = randomNumber(Math.floor(Math.random() * 10));
- }
- 
  
 
